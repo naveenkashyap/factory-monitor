7065c9e14080f0cd603feb5e758a2852355cddda
Naveen Kashyap <naveen@thekashyaps.net>
2018-01-19 18:22:43 -0800

    working product. Organized based on modular functionality. Cannot rpm package in this state.

--- config/config.py
+++ config/config.py
@@ -1,22 +1,27 @@
 import os
 import json
 
-dirpath = os.path.dirname(os.path.realpath(__file__))
-FILE = dirpath + '/config.json'
+FILE = os.path.dirname(os.path.realpath(__file__)) + '/config.json'
 
 class Config:
 	def __init__(self):
 		with open(FILE) as config_file:
 			self.data = json.load(config_file)
 
-	def get_schedd_status_metrics(self):
-		return self.data['schedd_status_metrics']
+	def get_schedd_status_location(self):
+		return self.data['schedd_status_location']
+	
+	def get_client_metrics(self):
+		return self.data['client_metrics']
 
-	def get_schedd_attrib_metrics(self):
-		return self.data['schedd_attrib_metrics']
+	def get_requested_metrics(self):
+		return self.data['requested_metrics']
+
+	def get_status_metrics(self):
+		return self.data['status_metrics']
 	
 	def get_database_url(self):
-		return self.data['databaseURL']
+		return self.data['database_URL']
 	
 	def get_database_username(self):
 		return self.data['username']
@@ -25,13 +30,10 @@ class Config:
 		return self.data['password']
 	
 	def get_database_name(self):
-		return self.data['databaseName']
+		return self.data['database_name']
 
 	def get_measurement_name(self):
-		return self.data['measurementName']
+		return self.data['measurement_name']
 
 	def get_current_factory_name(self):
-		try:
-			return os.environ['HOSTNAME'].split('.')[0]
-		except KeyError as e:
-			print e
+		return self.data['factory_name']
--- monitor.py
+++ monitor.py
@@ -1,19 +1,21 @@
-import aggregator 
-import messenger 
+from aggregator import aggregator 
+from messenger import messenger 
 from config import config
+import logging
 
 def main():
+
 	cfg = config.Config()
 	ag = aggregator.Aggregator(cfg)
 	msgr = messenger.InfluxMessenger(cfg)
 
 	factory_data = ag.aggregate_factory_data()
+	
 	if type(factory_data) is str:
 		msgr.push_failure(factory_data)
-	elif type(factory_data) is None:
-		print "factory_data is empty"
+	#elif type(factory_data) is None:
 	else:
-		msgr.push(factory_data)
+		msgr.push_data(factory_data)
 
 if __name__ == "__main__":
 	main()
